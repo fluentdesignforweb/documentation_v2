ec7f735569f47e4d197b7be596b3d692504a74c9
Shivendra Saurav <shivendrasaurav@gmail.com>
2021-10-20 13:10:02 +0530

    new sidenav

--- src/components/FooterComp.js
+++ src/components/FooterComp.js
@@ -1,13 +1,44 @@
 import React, {Component} from "react"
-import './fluent.css';
+import './footer.css';
 
 class Footer extends Component{
   
   render(){
     return(
-        <footer className="secondary_green">
-            <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Fork this project on <a href="https://github.com/fluentdesignforweb/fluentdesignforweb.github.io" className="pivot_button">GitHub</a><br /><br />
-        </footer>
+      <footer className="offset-large3 offset-medium4 large9 medium8 small12">
+        <div class="column lg-3 md-3 sm-9">
+          <p class="ta_center footerTitle">GitHub Actions</p>
+          <p class="ta_center footerLink">
+            <a href="#">Submit an Issue</a>
+            <a href="#">Star on Github</a>
+            <a href="#">Fork on Github</a>
+          </p>
+        </div>
+
+        <div class="column lg-3 md-3 sm-9">
+          <p class="ta_center footerTitle">Important Links</p>
+          <p class="ta_center footerLink">
+            <a href="https://fluentdesignforweb.github.io/documentation/">Docs 1.0.x</a>
+            <a href="https://fluentdesignforweb.github.io/documentation_v2/">Docs 2.0.x</a>
+            <a href="https://www.npmjs.com/package/fdweb">Installation</a>
+          </p>
+        </div>
+
+        <div class="column lg-3 md-3 sm-9">
+          <p class="ta_center footerTitle">Featured Projects</p>
+          <p class="ta_center footerLink">
+            <a href="https://shivendrasaurav.github.io/corona-rakshak/">Corona Rakshak</a>
+            <a href="https://hisabkitabapp.herokuapp.com/">Hisab Kitab</a>
+            <a href="https://revahack.com/">REVA HACK 2021</a>
+          </p>
+        </div>
+        <div class="column lg-7 md-7 sm-9 center">
+          <hr/>
+          <p>Free and Open Source</p>
+        </div>
+
+
+      </footer>
     );
   }
 
--- src/components/SidenavComp.js
+++ src/components/SidenavComp.js
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import './fluent.css';
 import '../App.css';
+import blob from "./blob-v2.png";
 import { NavLink } from "react-router-dom";
 
 class Sidenav extends Component{
@@ -10,7 +11,18 @@ class Sidenav extends Component{
         <nav className="column large3 medium4 small0">
             <div className="nav_content"><br /><br />
 
-                <h6 className="bold">Documentation v2</h6>
+            <a href="https://fluentdesignforweb.github.io/v2/" alt="home"><img src={blob} alt="blob" style={{width: "60px"}} /></a>
+                <h3 style={{display: "inline", position: "relative", top: "-20px", left: "10px"}}>Docs</h3>
+
+                <p className="bold" style={{lineHeight: "1"}}>Installation and Stuff</p>
+                <NavLink to="/newinv2">What's New</NavLink><br />
+                <NavLink to="/getstarted">Get Started</NavLink><br />
+                <NavLink to="/boilerplate">Boilerplate</NavLink><br />
+                <NavLink to="/styles">Styles</NavLink><br />
+                <NavLink to="/controls">Controls</NavLink><br />
+                <NavLink to="/darkmode">Dark Mode</NavLink><br />
+                <hr className="left" style={{border: "none", marginTop: "16px" ,marginBottom: "24px",  height: "1px", backgroundColor: "#1e1e1e", width: "60%"}}/>
+
 
                 <p className="bold" style={{lineHeight: "1"}}>Layout and More</p>
                 <NavLink to="/12colgrid">12 Col Grid</NavLink><br />
